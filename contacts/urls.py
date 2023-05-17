7c5ba2319030fee36a916f40af33cc2d66c247fa
abdrasheedm <rasheedvaniyannurrashi175@gmail.com>
2023-05-17 11:59:33 +0530

    removed check function

--- contacts/urls.py
+++ contacts/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('fetch-contacts/', views.fetch_contacts, name="fetch_contacts"),
-    path('check/', views.fetch, name="check"),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -63,26 +63,3 @@ def fetch_contacts(request):
     else:
         return JsonResponse({"error": "Failed to fetch contacts"})
 
-def fetch(request):
-    api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2NhdGlvbl9pZCI6IkpOVEpTV04ya0tkRVZkMElFbEZhIiwiY29tcGFueV9pZCI6ImJmb1Q3MkNWcm9oMlg4ZWZPUmdRIiwidmVyc2lvbiI6MSwiaWF0IjoxNjYxNDE2NzQzNTcxLCJzdWIiOiJQcVJEWDZqMjdXempXRUNsQm92eCJ9.u6WPtyudfB9R4nLnLbBZ6i9KquDeK6WnIOZxKAeE9Hg'
-    contacts_url = 'https://rest.gohighlevel.com/v1/contacts/'
-    field_name = 'DFS Booking Zoom Link'
-
-    headers = {
-        "Authorization" : f"Bearer {api_key}",
-        "Content-Type" : "application/json"
-    }
-    
-    # Fetching contacts
-    response = requests.get(contacts_url, headers=headers)
-    if response.status_code == 200:
-        contacts = response.json().get("contacts")
-        fields = []
-        for contact in contacts:
-            
-            if len(contact['customField']) > 0:
-                if contact['customField'][0]['value'] == field_name:
-                    fields.append(contact)
-        
-        return JsonResponse({"contacts": fields, "count": len(fields)})
-
